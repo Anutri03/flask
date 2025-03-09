7bebb3f046aa7ba549e50900178f35b03800cc88
Anutri03 <your.email@example.com>
2025-03-09 12:09:37 +0530

    Add login page template and update routing in app.py

--- app.py
+++ app.py
@@ -4,12 +4,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('login.html')
 
 
-@app.route('/landing2')
-def landin2():
-    return render_template('landing2.html')
+@app.route('/landingpagemain')
+def landingpagemain():
+    return render_template('landingpagemain.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/technology')
+def technology(): 
+    return render_template('technology.html')
+
+@app.route('/research')
+def research(): 
+    return render_template('research.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
